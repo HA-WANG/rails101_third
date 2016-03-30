9296358fbcd2bccd24103001c5b3ae97ea581d0f
HA-WANG <stone.wwf@gmail.com>
2016-03-30 18:19:47 -0500

    ch4 finish

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :find_group
+
+  def new
+    @post = @group.posts.new
+  end
+
+  def edit
+    @post = @group.posts.find(params[:id])
+  end
+
+  def create
+    @post = @group.posts.build(post_params)
+
+    if @post.save
+      redirect_to group_path(@group), notice: "Create success!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "Article update success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    @post.destroy
+    redirect_to group_path(@group), alert: "Post deleted!"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'groups#index'
-  resources :groups
+  resources :groups do
+    resources :posts
+  end
 end
