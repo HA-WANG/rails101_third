87c78dca34c162f8f41602afc13adb36b91d0c07
HA-WANG <stone.wwf@gmail.com>
2016-03-31 17:38:09 -0500

    set fake data and seeds

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'bootstrap-sass'
 gem 'simple_form'
 gem "devise", "~> 3.4.1"
+gem 'faker', '1.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Create fake data"
+
+# Create 30 accounts
+30.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "12345678"
+  User.create!(email: email,
+               name: name,
+               password: password,
+               password_confirmation: password)
+end
+
+# account 1, create 20 groups, each group 30 posts
+for i in 1..30 do
+  title = Faker::Hipster.word
+  description = Faker::Hipster.sentence
+  Group.create!([title: title, description: description, user_id: "1"])
+  GroupUser.create(group_id: i, user_id: 1)
+  for k in 1..50 do
+    content = Faker::Hipster.paragraph
+    Post.create!([group_id: "#{i}",content: content, user_id: "1"])
+  end
+end
+
+puts "Complete"
